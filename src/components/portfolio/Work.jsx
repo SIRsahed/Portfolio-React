bfe7c386a106931d0cb3a6e6a9ee7b9f00f5ca6e
sirsahed <sahedrahmanshakil@gmail.com>
2024-07-10 15:17:44 +0600

    react-portfolio

--- src/components/portfolio/Work.jsx
+++ src/components/portfolio/Work.jsx
@@ -3,6 +3,7 @@ import { Container, Row, Col } from 'react-bootstrap'
 import "./work.css"
 import Port1 from "../../assets/port1.png"
 import Port3 from "../../assets/port3.png"
+import Agency from "../../assets/agency.png"
 import Port9 from "../../assets/port9.png"
 import { Tabs, Tab } from 'react-bootstrap'
 import Workreusable from '../workreusable/Workreusable'
@@ -12,6 +13,7 @@ import Slider from "react-slick";
 import Reviewreusable from '../reviewreusable/Reviewreusable'
 import RazibSir from "../../assets/Razibsir.jpeg"
 import Orebi from "../../assets/orebi.png"
+import NewsPro from "../../assets/newspro.png"
 
 const Work = () => {
   var settings = {
@@ -36,51 +38,52 @@ const Work = () => {
           </div>
         </Row>
         <Row className='text-center'>
-          <Tabs defaultActiveKey="business" className="mb-4 list11">
-            <Tab eventKey="business" title="Business">
-              <Row>
-                <Workreusable image={Port1} title="Finsweet Multipage" content="Finsweet Agency's website contains 12 pages built with Bootstrap. Responsive designed to work smoothly on different devices" github="https://github.com/SIRsahed/Finsweet-10Pages" livesite="https://sirsahed.github.io/Finsweet-10Pages/" />
-
-                <Workreusable image={Port3} title="Emprise Landing Page" content="This is a travel agency website named 'Emprise' with a variety of hovers and animations added. It is a Nepal Based Travel Agency website I" github="https://github.com/SIRsahed/Nepali-Travel-Agency" livesite="https://sirsahed.github.io/Nepali-Travel-Agency/"/>
-
-                <Workreusable image={Port9} title="QWERy Indonesia" content="This was my first project when I was just learnt basic HTML and CSS. I designed this landing using only HTML and CSS, not responsive. I" github="https://github.com/SIRsahed/Petro-Oil-Tailwind" livesite="https://github.com/SIRsahed/Petro-Oil-Tailwind" />
-
-              </Row>
-            </Tab>
-            <Tab eventKey="portfolio" title="Portfolio">
-              <Row>
-                <Workreusable image={Port1} title="Finsweet Multipage" content="Finsweet Agency's website contains 12 pages built with Bootstrap. Responsive designed to work smoothly on different devices" github="Git Hub" livesite="Live Site" />
-
-                <Workreusable image={Port3} title="Emprise Landing Page" content="This is a travel agency website named 'Emprise' with a variety of hovers and animations added. It is a Nepal Based Travel Agency website I" github="Git Hub" livesite="Live Site" />
-              </Row>
-            </Tab>
-            <Tab eventKey="education" title="Education">
-              <Row>
-                <Workreusable image={Port1} title="Finsweet Multipage" content="Finsweet Agency's website contains 12 pages built with Bootstrap. Responsive designed to work smoothly on different devices using Bootstrap V5.3 I" github="Git Hub" livesite="Live Site" />
-
-              </Row>
-            </Tab>
+          <Tabs defaultActiveKey="ecommerce" className="mb-4 list11">
             <Tab eventKey="ecommerce" title="E-Commerce">
               <Row>
                 <Workreusable image={Orebi} title="Finsweet Multipage" content="Orebi is a fully functional e-commerce site with a user-friendly interface and product management, built with modern technologies." github="https://github.com/SIRsahed/E-commerce" livesite="https://oreby-ecommerce.netlify.app/" />
               </Row>
             </Tab>
-            <Tab eventKey="blog" title="Blog">
+            <Tab eventKey="business" title="Business">
               <Row>
-                <Workreusable image={Port1} title="Finsweet Multipage" content="Finsweet Agency's website contains 12 pages built with Bootstrap. Responsive designed to work smoothly on different devices using Bootstrap V5.3 I" github="Git Hub" livesite="Live Site" />
+                <Workreusable image={Port1} title="Finsweet Multipage" content="Finsweet Agency's website contains 12 pages built with Bootstrap. Responsive designed to work smoothly on different devices I" github="https://github.com/SIRsahed/Finsweet-10Pages" livesite="https://sirsahed.github.io/Finsweet-10Pages/" />
 
-                <Workreusable image={Port3} title="Emprise Landing Page" content="This is a travel agency website named 'Emprise' with a variety of hovers and animations added. It is a Nepal Based Travel Agency website I" github="Git Hub" livesite="Live Site" />
+                <Workreusable image={Port3} title="Emprise Landing Page" content="This is a travel agency website named 'Emprise' with a variety of hovers and animations added. It is a Nepal Based Travel Agency website I" github="https://github.com/SIRsahed/Nepali-Travel-Agency" livesite="https://sirsahed.github.io/Nepali-Travel-Agency/" />
 
-                <Workreusable image={Port9} title="QWERy Indonesia" content="This was my first project when I was just learnt basic HTML and CSS. I designed this landing using only HTML and CSS, not responsive. I" github="Git Hub" livesite="Live Site" />
+                <Workreusable image={Agency} title="BWFC Agency" content="BWFC is a stylish agency website built with React and Bootstrap. It features clean design and comprehensive service details I" github="https://github.com/SIRsahed/Petro-Oil-Tailwind" livesite="https://github.com/SIRsahed/Petro-Oil-Tailwind" />
+
+              </Row>
+            </Tab>
+            <Tab eventKey="newspaper" title="Newspaper">
+              <Row>
+                <Workreusable image={NewsPro} title="NewsPro" content="It's a newspaper website's template. It contains various contents and attractive styles. User interface is so attractive I" github="https://github.com/SIRsahed/NewsPro" livesite="https://sirsahed.github.io/NewsPro/" />
+              </Row>
+            </Tab>
+            <Tab eventKey="portfolio" title="Portfolio">
+              <Row>
+                <Workreusable image={Port1} title="Finsweet Multipage" content="Finsweet Agency's website contains 12 pages built with Bootstrap. Responsive designed to work smoothly on different devices I" github="https://github.com/SIRsahed/Finsweet-10Pages" livesite="https://sirsahed.github.io/Finsweet-10Pages/" />
+
+                <Workreusable image={Port3} title="Emprise Landing Page" content="This is a travel agency website named 'Emprise' with a variety of hovers and animations added. It is a Nepal Based Travel Agency website I" github="https://github.com/SIRsahed/Nepali-Travel-Agency" livesite="https://sirsahed.github.io/Nepali-Travel-Agency/" />
+
+                <Workreusable image={Agency} title="BWFC Agency" content="BWFC is a stylish agency website built with React and Bootstrap. It features clean design and comprehensive service details I" github="https://github.com/SIRsahed/Petro-Oil-Tailwind" livesite="https://github.com/SIRsahed/Petro-Oil-Tailwind" />
+              </Row>
+            </Tab>
+            <Tab eventKey="education" title="Education">
+              <Row>
+                <Workreusable image={Port1} title="Finsweet Multipage" content="Finsweet Agency's website contains 12 pages built with Bootstrap. Responsive designed to work smoothly on different devices I" github="https://github.com/SIRsahed/Finsweet-10Pages" livesite="https://sirsahed.github.io/Finsweet-10Pages/" />
+
+                <Workreusable image={Port3} title="Emprise Landing Page" content="This is a travel agency website named 'Emprise' with a variety of hovers and animations added. It is a Nepal Based Travel Agency website I" github="https://github.com/SIRsahed/Nepali-Travel-Agency" livesite="https://sirsahed.github.io/Nepali-Travel-Agency/" />
+
+                <Workreusable image={Agency} title="BWFC Agency" content="BWFC is a stylish agency website built with React and Bootstrap. It features clean design and comprehensive service details I" github="https://github.com/SIRsahed/Petro-Oil-Tailwind" livesite="https://github.com/SIRsahed/Petro-Oil-Tailwind" />
               </Row>
             </Tab>
             <Tab eventKey="restaurant" title="Restaurant">
               <Row>
-                <Workreusable image={Port1} title="Finsweet Multipage" content="Finsweet Agency's website contains 12 pages built with Bootstrap. Responsive designed to work smoothly on different devices using Bootstrap V5.3 I" github="Git Hub" livesite="Live Site" />
+                <Workreusable image={Port1} title="Finsweet Multipage" content="Finsweet Agency's website contains 12 pages built with Bootstrap. Responsive designed to work smoothly on different devices I" github="https://github.com/SIRsahed/Finsweet-10Pages" livesite="https://sirsahed.github.io/Finsweet-10Pages/" />
 
-                <Workreusable image={Port3} title="Emprise Landing Page" content="This is a travel agency website named 'Emprise' with a variety of hovers and animations added. It is a Nepal Based Travel Agency website I" github="Git Hub" livesite="Live Site" />
+                <Workreusable image={Port3} title="Emprise Landing Page" content="This is a travel agency website named 'Emprise' with a variety of hovers and animations added. It is a Nepal Based Travel Agency website I" github="https://github.com/SIRsahed/Nepali-Travel-Agency" livesite="https://sirsahed.github.io/Nepali-Travel-Agency/" />
 
-                <Workreusable image={Port9} title="QWERy Indonesia" content="This was my first project when I was just learnt basic HTML and CSS. I designed this landing using only HTML and CSS, not responsive. I" github="Git Hub" livesite="Live Site" />
+                <Workreusable image={Agency} title="BWFC Agency" content="BWFC is a stylish agency website built with React and Bootstrap. It features clean design and comprehensive service details I" github="https://github.com/SIRsahed/Petro-Oil-Tailwind" livesite="https://github.com/SIRsahed/Petro-Oil-Tailwind" />
               </Row>
             </Tab>
           </Tabs>
